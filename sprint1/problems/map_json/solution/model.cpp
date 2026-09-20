9666b849fbe7117305642770036413b9942071de
Williamsonskiy <danytolstov@yandex.ru>
2026-09-20 14:57:58 +0300

    Update model.cpp

--- sprint1/problems/map_json/solution/model.cpp
+++ sprint1/problems/map_json/solution/model.cpp
@@ -1,20 +1,21 @@
 #include "model.h"
+
 #include <stdexcept>
 
 namespace model {
-
-using namespace std::string_literals;
+using namespace std::literals;
 
 void Map::AddOffice(Office office) {
     if (warehouse_id_to_index_.contains(office.GetId())) {
-        throw std::invalid_argument("Duplicate warehouse"s);
+        throw std::invalid_argument("Duplicate warehouse to assign");
     }
+
     const size_t index = offices_.size();
-    auto [it, inserted] = warehouse_id_to_index_.emplace(office.GetId(), index);
+    Office& o = offices_.emplace_back(std::move(office));
     try {
-        offices_.push_back(std::move(office));
+        warehouse_id_to_index_.emplace(o.GetId(), index);
     } catch (...) {
-        warehouse_id_to_index_.erase(it);
+        offices_.pop_back();
         throw;
     }
 }
@@ -23,13 +24,14 @@ void Game::AddMap(Map map) {
     const size_t index = maps_.size();
     if (auto [it, inserted] = map_id_to_index_.emplace(map.GetId(), index); !inserted) {
         throw std::invalid_argument("Map with id "s + *map.GetId() + " already exists"s);
-    }
-    try {
-        maps_.push_back(std::move(map));
-    } catch (...) {
-        map_id_to_index_.erase(map.GetId());
-        throw;
+    } else {
+        try {
+            maps_.emplace_back(std::move(map));
+        } catch (...) {
+            map_id_to_index_.erase(it);
+            throw;
+        }
     }
 }
 
-}  // namespace model
+}
