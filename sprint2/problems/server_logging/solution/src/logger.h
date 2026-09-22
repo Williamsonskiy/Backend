56578f18ef5b3a9d128bfc0bb214ce1a4c370ac8
Williamsonskiy <danytolstov@yandex.ru>
2026-09-22 17:37:58 +0300

    Update logger.h

--- sprint2/problems/server_logging/solution/src/logger.h
+++ sprint2/problems/server_logging/solution/src/logger.h
@@ -1,16 +1,15 @@
 #pragma once
 
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/json.hpp>
 #include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/keywords/format.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/manipulators/add_value.hpp>
-#include <iostream>
+#include <boost/json.hpp>
 #include <string_view>
+#include <optional>
+#include <string>
 
 namespace logger {
 
@@ -19,85 +18,83 @@ namespace keywords = boost::log::keywords;
 namespace json = boost::json;
 
 BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData", json::value)
-BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 
-inline void JsonFormatter(logging::record_view const& rec, logging::formatting_ostream& strm) {
-    json::object log_obj;
+inline void JsonFormatter(const logging::record_view& rec, logging::basic_formatting_ostream<char>& strm) {
+    json::object root;
 
-    if (auto ts = rec[timestamp]) {
-        log_obj["timestamp"] = boost::posix_time::to_iso_extended_string(*ts);
+    // Обязательные поля timestamp и message
+    root["timestamp"] = logging::extract<std::string>("TimeStamp", rec).get_value_or("");
+    root["message"] = *rec[logging::expressions::smessage];
+
+    // Поле data (если передано через атрибут AdditionalData)
+    if (auto data_val = rec[additional_data]) {
+        root["data"] = *data_val;
+    } else {
+        root["data"] = json::object{};
     }
 
-    if (auto data = rec[additional_data]) {
-        log_obj["data"] = *data;
-    }
-
-    log_obj["message"] = *rec[logging::expressions::smessage];
-
-    strm << json::serialize(log_obj);
+    strm << json::serialize(root);
 }
 
-inline void InitLogger() {
+inline void InitBoostLogFilter() {
     logging::add_common_attributes();
-
     logging::add_console_log(
         std::clog,
         keywords::format = &JsonFormatter,
-        keywords::auto_flush = true
+        keywords::auto_flush = true // КРИТИЧНО: сбрасываем буфер сразу, чтобы автотесты не получали пустые строки
     );
 }
 
-inline void LogServerStarted(int port, const std::string& address) {
-    json::value data{
-        {"port", port},
-        {"address", address}
-    };
-    BOOST_LOG_TRIVIAL(info) << logging::add_value(additional_data, data)
-                            << "server started";
+// Лог старта сервера
+inline void LogServerStarted(const std::string& address, unsigned int port) {
+    json::object data;
+    data["address"] = address;
+    data["port"] = port;
+
+    BOOST_LOG_TRIVIAL(info) 
+        << logging::add_value(additional_data, data) 
+        << "server started";
 }
 
+// Лог завершения работы сервера
 inline void LogServerExited(int code, const std::optional<std::string>& exception_msg = std::nullopt) {
-    json::object data_obj;
-    data_obj["code"] = code;
+    json::object data;
+    data["code"] = code;
     if (exception_msg) {
-        data_obj["exception"] = *exception_msg;
+        data["exception"] = *exception_msg;
     }
-    BOOST_LOG_TRIVIAL(info) << logging::add_value(additional_data, json::value(data_obj))
-                            << "server exited";
+
+    BOOST_LOG_TRIVIAL(info) 
+        << logging::add_value(additional_data, data) 
+        << "server exited";
 }
 
-inline void LogNetworkError(int code, std::string_view text, std::string_view where) {
-    json::value data{
-        {"code", code},
-        {"text", std::string(text)},
-        {"where", std::string(where)}
-    };
-    BOOST_LOG_TRIVIAL(info) << logging::add_value(additional_data, data)
-                            << "error";
+// Лог входящего запроса
+inline void LogRequest(std::string_view ip, std::string_view URI, std::string_view method) {
+    json::object data;
+    data["ip"] = std::string(ip);
+    data["URI"] = std::string(URI);
+    data["method"] = std::string(method);
+
+    BOOST_LOG_TRIVIAL(info) 
+        << logging::add_value(additional_data, data) 
+        << "request received";
 }
 
-inline void LogRequest(std::string_view ip, std::string_view uri, std::string_view method) {
-    json::value data{
-        {"ip", std::string(ip)},
-        {"URI", std::string(uri)},
-        {"method", std::string(method)}
-    };
-    BOOST_LOG_TRIVIAL(info) << logging::add_value(additional_data, data)
-                            << "request received";
-}
-
-inline void LogResponse(std::string_view ip, long long response_time, int code, const std::optional<std::string>& content_type) {
-    json::object data_obj;
-    data_obj["ip"] = std::string(ip);
-    data_obj["response_time"] = response_time;
-    data_obj["code"] = code;
+// Лог отправленного ответа
+inline void LogResponse(long long response_time, int code, const std::optional<std::string>& content_type) {
+    json::object data;
+    data["response_time"] = response_time;
+    data["code"] = code;
     if (content_type) {
-        data_obj["content_type"] = *content_type;
+        data["content_type"] = *content_type;
     } else {
-        data_obj["content_type"] = nullptr;
+        data["content_type"] = "null";
     }
-    BOOST_LOG_TRIVIAL(info) << logging::add_value(additional_data, json::value(data_obj))
-                            << "response sent";
+
+    BOOST_LOG_TRIVIAL(info) 
+        << logging::add_value(additional_data, data) 
+        << "response sent";
 }
 
-}  // namespace logger
+} // namespace logger
