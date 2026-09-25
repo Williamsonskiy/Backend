0e9da9993158317b66b10ae1571afaf927a2bf1b
Williamsonskiy <danytolstov@yandex.ru>
2026-09-25 18:27:59 +0300

    Update shoot.py

--- sprint3/problems/flamegraph/solution/shoot.py
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -1,8 +1,10 @@
 import argparse
-import subprocess
-import time
+import os
 import random
 import shlex
+import signal
+import subprocess
+import time
 
 RANDOM_LIMIT = 1000
 SEED = 123456789
@@ -47,8 +49,63 @@ def make_shots():
     print('Shooting complete')
 
 
-server = run(start_server())
-make_shots()
-stop(server)
-time.sleep(1)
-print('Job done')
+def build_flamegraph():
+    # 1. Распаковываем сырые данные perf
+    perf_script = subprocess.Popen(
+        ['perf', 'script', '-i', 'perf.data'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL
+    )
+
+    # 2. Сворачиваем стеки вызовов с помощью Perl-скрипта stackcollapse-perf.pl
+    stackcollapse_path = os.path.join('.', 'FlameGraph', 'stackcollapse-perf.pl')
+    stackcollapse = subprocess.Popen(
+        ['perl', stackcollapse_path],
+        stdin=perf_script.stdout,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL
+    )
+    perf_script.stdout.close()
+
+    # 3. Генерируем итоговый graph.svg с помощью flamegraph.pl
+    flamegraph_path = os.path.join('.', 'FlameGraph', 'flamegraph.pl')
+    with open('graph.svg', 'w') as svg_file:
+        flamegraph = subprocess.Popen(
+            ['perl', flamegraph_path],
+            stdin=stackcollapse.stdout,
+            stdout=svg_file,
+            stderr=subprocess.DEVNULL
+        )
+        stackcollapse.stdout.close()
+        flamegraph.communicate()
+
+    print('Flamegraph generated')
+
+
+if __name__ == '__main__':
+    # 1. Запускаем сервер
+    server_cmd = start_server()
+    server = run(server_cmd)
+    
+    # Небольшая пауза, чтобы сервер успел инициализироваться
+    time.sleep(1)
+
+    # 2. Запускаем perf record с привязкой к PID сервера
+    perf_cmd = f'perf record -g -p {server.pid} -o perf.data'
+    perf = run(perf_cmd)
+    time.sleep(0.5)
+
+    # 3. Выполняем обстрел запросами
+    make_shots()
+
+    # 4. Корректно останавливаем perf record, чтобы сбросить буфер в perf.data
+    perf.send_signal(signal.SIGINT)
+    perf.wait()
+
+    # 5. Останавливаем сервер
+    stop(server)
+
+    # 6. Строим флеймграф
+    build_flamegraph()
+
+    print('Job done')
